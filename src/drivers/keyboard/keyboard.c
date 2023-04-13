181834180861f50fa507a62168dddefa3f0b9084
skejeton <ishidex2@gmail.com>
2023-04-13 20:33:04 +0400

    connect keyboard driver to irq (broken)

--- src/drivers/keyboard/keyboard.c
+++ src/drivers/keyboard/keyboard.c
@@ -1,9 +1,14 @@
 #include "keyboard.h"
 #include "../../io.h"
+#include "../../mem.h"
 
 // KEYBOARD DRIVER
 // TODO: Handle fake shifts properly
 // TODO: Handle keypad
+
+static keyboard GlobalKeyboard;
+static keyboard_key GlobalKeys[128];
+static size_t GlobalKeysCount;
 
 const char TransLo[128] = {
     0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, '\t', 
@@ -34,71 +39,87 @@ uint8_t TransformCharcode(keyboard *Kbd, uint8_t Key) {
     }
     return TransLo[Key];
 }
-
+#if 0
 static uint8_t IsFull() {
     return IO_In8(0x64)&1;
 }
+#endif
 
 static uint8_t ReadKey() {
-    uint8_t Ret = 0;
-    
-    if (IsFull()) {
-        Ret = IO_In8(0x60);
-    }
-    
-    return Ret;
+    return IO_In8(0x60);
 }
 
-void Keyboard_CollectEvents(keyboard *Kbd, keyboard_key *Keys, uint32_t KeysMax, uint32_t *KeysLen) { 
-    *KeysLen = 0;
+static void Keyboard_HandleInterrupt_Internal(keyboard *Kbd, keyboard_key *Keys, uint32_t KeysMax, uint32_t *KeysLen)
+{
     uint8_t Alternative = 0;
-    
-    while (*KeysLen < KeysMax) {
-        uint8_t K = ReadKey(Kbd);
-        if (K == 0xE0) {
-            Alternative = 1;
-            continue;
-        }
-        if (K == 0xAA) {
-            Kbd->LShift = 0;
-            continue;
-        }
-        
-        uint8_t released = K >= 128;
-        if (released) continue;
-        if (Alternative) K += 128;
-    
-        switch (K) {
-            case KEY_LSHIFT: case KEY_FKLSHIFT: 
-                Kbd->LShift = !released; 
-                break;
-            case KEY_RSHIFT: case KEY_FKRSHIFT:
-                Kbd->RShift = !released; 
-                break;
-            case KEY_LCTRL:       Kbd->LCtrl = !released; break;
-            case KEY_RCTRL:       Kbd->RCtrl = !released; break;
-            case KEY_LALT:        Kbd->LAlt = !released; break;
-            case KEY_RALT:        Kbd->RAlt = !released; break;
-            case KEY_CAPS_LOCK:   if (!released) Kbd->CapsLock = !Kbd->CapsLock; break;
-        
-            case 0: return; // ran out of input
-        }
-    
-        Keys[(*KeysLen)++] = (keyboard_key){
-            Kbd->CapsLock,
-            Kbd->LShift,
-            Kbd->RShift,
-            Kbd->LCtrl,
-            Kbd->RCtrl,
-            Kbd->LAlt,
-            Kbd->RAlt,
-            Kbd->LShift||Kbd->RShift,
-            Kbd->LCtrl||Kbd->RCtrl,
-            Kbd->LAlt||Kbd->RAlt,
-            released, K,
-            TransformCharcode(Kbd, K),
-            K < 128 ? TransLo[K] : 0
-        };
-        Alternative = 0;
+    if (*KeysLen >= KeysMax) {
+        return;
     }
+     
+    uint8_t K = ReadKey();
+    if (K == 0xE0) {
+        Alternative = 1;
+        K = ReadKey();
+    }
+    if (K == 0xAA) {
+        Kbd->LShift = 0;
+        return;
+    }
+        
+    uint8_t Released = K >= 128;
+    if (Released) return;
+    if (Alternative) K += 128;
+    
+    switch (K) {
+        case KEY_LSHIFT: case KEY_FKLSHIFT: 
+            Kbd->LShift = !Released; 
+            break;
+        case KEY_RSHIFT: case KEY_FKRSHIFT:
+            Kbd->RShift = !Released; 
+            break;
+        case KEY_LCTRL:       Kbd->LCtrl = !Released; break;
+        case KEY_RCTRL:       Kbd->RCtrl = !Released; break;
+        case KEY_LALT:        Kbd->LAlt = !Released; break;
+        case KEY_RALT:        Kbd->RAlt = !Released; break;
+        case KEY_CAPS_LOCK:   if (!Released) Kbd->CapsLock = !Kbd->CapsLock; break;
+        
+        case 0: return; // ran out of input
+    }
+    
+    Keys[(*KeysLen)++] = (keyboard_key){
+        Kbd->CapsLock,
+        Kbd->LShift,
+        Kbd->RShift,
+        Kbd->LCtrl,
+        Kbd->RCtrl,
+        Kbd->LAlt,
+        Kbd->RAlt,
+        Kbd->LShift||Kbd->RShift,
+        Kbd->LCtrl||Kbd->RCtrl,
+        Kbd->LAlt||Kbd->RAlt,
+        Released, K,
+        TransformCharcode(Kbd, K),
+        K < 128 ? TransLo[K] : 0
+    };
+    Alternative = 0;
+}
+
+void Keyboard_HandleInterrupt()
+{
+    Keyboard_HandleInterrupt_Internal(&GlobalKeyboard, GlobalKeys, 128, &GlobalKeysCount);
+}
+
+void Keyboard_CollectEvents(keyboard *Kbd, keyboard_key *Keys, uint32_t KeysMax, uint32_t *KeysLen)
+{ 
+    size_t KeysToCopy = KeysMax;
+    if (GlobalKeysCount < KeysToCopy) {
+        KeysToCopy = GlobalKeysCount;
+    }
+
+    memcpy(Kbd, &GlobalKeyboard, sizeof GlobalKeyboard);
+    memcpy(Keys, &GlobalKeys, sizeof(keyboard_key) * KeysToCopy);
+
+    *KeysLen = GlobalKeysCount;
+    //ConPrintf("Keys: %d\n", (int)GlobalKeysCount);
+    GlobalKeysCount = 0;
 }
---
+++ src/mem.h
@@ -0,0 +1,13 @@
+#ifndef H_BOS_MEM
+#define H_BOS_MEM
+
+#include <stddef.h>
+
+static inline memcpy(uint8_t *Destination, uint8_t *Source, size_t N)
+{
+    while (N--) {
+        *Source++ = *Destination++;
+    }
+}
+
+#endif // H_BOS_MEM
